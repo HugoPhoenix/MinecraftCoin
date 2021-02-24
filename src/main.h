3ab8ddfacb25e69f263ba5e482f529143a4a280e
HugoPhoenix <77645698+HugoPhoenix@users.noreply.github.com>
2021-02-24 19:26:09 +1030

    Update main.h

--- src/main.h
+++ src/main.h
@@ -25,7 +25,7 @@ class CReserveKey;
 class CWallet;
 
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
-static const unsigned int MAX_BLOCK_SIZE = 8000000;
+static const unsigned int MAX_BLOCK_SIZE = 1000000;
 /** The maximum size for mined blocks */
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 /** The maximum size for transactions we're willing to relay/mine **/
